dca7bd5ea07cd2201dcab7026d86ed34e40da041
Andrew Medeiros <andrew@amedeiros.com>
2019-08-22 20:58:57 -0400

    Short positioning

--- stocking.py
+++ stocking.py
@@ -26,13 +26,33 @@ def years_from_now(years=1):
     return future.strftime("%Y-%m-%d")
 
 
-def postion_sizing(risk, price, stop, exit=1):
+def postion_sizing_long(risk, price, stop, exit):
     max_loss_per_share = price - stop
     position_size = _round_down(risk / max_loss_per_share)
     max_gain = position_size * exit
     total_risk = position_size * max_loss_per_share
     cost = position_size * price
     profit = max_gain - cost
+    risk_reward_ratio = ((profit / total_risk) * 100) / 100
+    df = pd.DataFrame({
+        'position_size': [position_size],
+        'max_loss_per_share': [max_loss_per_share],
+        'total_risk': [total_risk],
+        'total_gain': [max_gain],
+        'profit': [profit],
+        'purchase_cost': [cost],
+        'risk_reward_ratio': [risk_reward_ratio]})
+
+    return df
+
+
+def position_sizing_short(risk, price, stop, exit):
+    max_loss_per_share = stop - price
+    position_size = _round_down(risk / max_loss_per_share)
+    max_gain = position_size * exit
+    total_risk = position_size * max_loss_per_share
+    cost = position_size * price
+    profit = cost - max_gain
     risk_reward_ratio = ((profit / total_risk) * 100) / 100
     df = pd.DataFrame({
         'position_size': [position_size],
