b7edbbd1fa37f6d270843ea1f4fec397a019eebf
Andrew Medeiros <andrew@amedeiros.com>
2019-09-21 21:52:13 -0400

    Update alpha vantage client

--- alpha_vantage_client.py
+++ alpha_vantage_client.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class AlphaVantageClient:
-    BASE_URL = 'https://www.alphavantage.co'
+    BASE_URL = 'https://www.alphavantage.co/query?'
     KEY = os.environ['ALPHA_VANTAGE_KEY']
 
     def __init__(self, ticker):
@@ -13,20 +13,21 @@ class AlphaVantageClient:
 
     def time_series_daily(self, outputsize='full'):
         response = requests.get(
-            self._url(function='TIME_SERIES_DAILY', outputsize=outputsize))
+            self._url(function='TIME_SERIES_DAILY', outputsize=outputsize, symbol=self.ticker))
 
         return self._frame_response(response, 'Time Series (Daily)')
 
     def time_series_intraday(self, interval='60min', outputsize='full'):
         url = self._url(function='TIME_SERIES_INTRADAY',
-                        outputsize=outputsize) + '&interval=' + interval
+                        outputsize=outputsize, interval=interval, symbol=self.ticker)
+
         response = requests.get(url)
 
         return self._frame_response(response, 'Time Series (%s)' % (interval))
 
     def sma(self, interval='daily', series_type='open', time_period='50'):
-        url = self._url(function='SMA') + '&series_type=' + \
-            series_type + '&time_period=' + time_period + '&interval=' + interval
+        url = self._url(function='SMA', symbol=self.ticker,
+                        series_type=series_type, time_period=time_period, interval=interval)
 
         response = requests.get(url)
 
@@ -36,33 +37,70 @@ class AlphaVantageClient:
             try:
                 data['dates'].append(datetime.strptime(k, "%Y-%m-%d"))
                 data['sma'].append(float(v['SMA']))
-            except Exception as ex:
-                next
+            except:
+                pass
 
-        df = pd.DataFrame(data=data)
+        return pd.DataFrame(data=data)
 
-        return df
+    def sector_performance(self):
+        url = self._url(function="SECTOR")
 
-    def _url(self, function, outputsize=None):
-        if outputsize:
-            return '%s/query?function=%s&symbol=%s&outputsize=%s&apikey=%s' % (
-                AlphaVantageClient.BASE_URL, function, self.ticker, outputsize, AlphaVantageClient.KEY)
+        response = requests.get(url)
 
-        return '%s/query?function=%s&symbol=%s&apikey=%s' % (
-            AlphaVantageClient.BASE_URL, function, self.ticker, AlphaVantageClient.KEY)
+        data = {'type': [], 'energy': [], 'consumer discretionary': [],
+                'industrials': [], 'consumer staples': [], 'financials': [],
+                'materials': [], 'health care': [], 'communication': [],
+                'real estate': [], 'IT': [], 'utilities': []}
+
+        for k, v in response.json().items():
+            if k != 'Meta Data':
+                data['type'].append(k)
+                data['energy'].append(float(v['Energy'][:-1]))
+                data['consumer discretionary'].append(
+                    float(v['Consumer Discretionary'][:-1]))
+                data['industrials'].append(float(v['Industrials'][:-1]))
+                data['consumer staples'].append(
+                    float(v['Consumer Staples'][:-1]))
+                data['financials'].append(float(v['Financials'][:-1]))
+                data['materials'].append(float(v['Materials'][:-1]))
+                data['health care'].append(float(v['Health Care'][:-1]))
+                data['communication'].append(
+                    float(v['Communication Services'][:-1]))
+                data['IT'].append(float(v['Information Technology'][:-1]))
+                data['utilities'].append(float(v['Utilities'][:-1]))
+
+                if 'Real Estate' in v.keys():
+                    data['real estate'].append(float(v['Real Estate'][:-1]))
+                else:
+                    # real estate not always present
+                    data['real estate'].append(0.0)
+
+        return pd.DataFrame(data=data)
+
+    def _url(self, **vargs):
+        url = AlphaVantageClient.BASE_URL
+
+        for k, v in vargs.items():
+            url += "%s=%s&" % (k, v)
+
+        url += 'apikey=' + AlphaVantageClient.KEY
+
+        return url
 
     def _frame_response(self, response, primary_key):
         data = {'dates': [], 'open': [], 'close': [],
                 'high': [], 'low': [], 'volume': []}
 
         for k, v in response.json()[primary_key].items():
-            data['dates'].append(datetime.strptime(k, "%Y-%m-%d"))
+            try:
+                data['dates'].append(datetime.strptime(k, "%Y-%m-%d"))
+            except:
+                data['dates'].append(datetime.strptime(k, "%Y-%m-%d %H:%M:%S"))
+
             data['close'].append(float(v['4. close']))
             data['open'].append(float(v['1. open']))
             data['high'].append(float(v['2. high']))
             data['low'].append(float(v['3. low']))
             data['volume'].append(float(v['5. volume']))
 
-        df = pd.DataFrame(data=data)
-
-        return df
+        return pd.DataFrame(data=data)
