b333aeec9ae44cebf4a60cc9c0e14fb76ae17efa
Andrew Medeiros <andrew@amedeiros.com>
2021-01-25 13:24:20 -0500

    Celery setup

--- algo_bot/worker/app.py
+++ algo_bot/worker/app.py
@@ -4,4 +4,6 @@ from algo_bot.settings import AlgoBotSettings, get_settings
 
 settings: AlgoBotSettings = get_settings()
 
-app = Celery(broker=f"redis://{settings.redids_host}:{settings.redis_port}/0")
+app = Celery(broker=f"redis://{settings.redis_host}:{settings.redis_port}/0")
+
+app.autodiscover_tasks(["algo_bot.worker.tasks"])
